a04ac44cbdc0e2c6c8292647f0195029594c0aed
NATNAEL BIRHANU <negusnati@gmail.com>
2022-05-24 06:44:57 -0700

    index route updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
+use Prophecy\Doubler\Generator\Node\ReturnTypeNode;
 
 class HomeController extends Controller
 {
@@ -22,4 +23,8 @@ class HomeController extends Controller
             return redirect()->back();
         }
     }
+     public function index()
+     {
+        return view('user.home');
+     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',[HomeController::class,'index']);
 
 Route::get('/home',[HomeController::class,'redirect']);
 
